89bea0f0eae10078682cc9355f6180e2d429d1b0
flavioafsantana <flavioafsantana@gmail.com>
2024-12-04 23:44:12 -0300

    Atualiza .gitignore para ignorar configurações da IDE e arquivos desnecessários

---
+++ src/ConversorDeMoedas.java
@@ -0,0 +1,10 @@
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+public class ConversorDeMoeda {
+    private final String chaveApi;
+}
